3581ff6833294be174e5a691c834a40bff00c5ac
Hakan Ayhan <hakanayhnn@gmail.com>
2023-03-20 20:11:58 +0300

    +customer manager: customer spawner

--- Assets/Scripts/Systems/CustomerManager.cs
+++ Assets/Scripts/Systems/CustomerManager.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class CustomerManager : MonoBehaviour
 {
- 
+    [SerializeField] private GameObject _customerPrefab;
+
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private float _maxCustomer;
+    private float _currentCustomer = 0;
+
+    private void Update()
+    {
+        if (_currentCustomer < _maxCustomer)
+        {
+            SpawnNewCustomer();
+        }
+    }
+
+    private void SpawnNewCustomer()
+    {
+        GameObject customerGameObject = Instantiate(_customerPrefab, _spawnPoint.position, Quaternion.identity);
+        _currentCustomer++;
+    }
 }
