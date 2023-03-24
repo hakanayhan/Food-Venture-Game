3827d8afaee43b957e336bcb61a8c311f5411390
Hakan Ayhan <hakanayhnn@gmail.com>
2023-03-24 20:40:48 +0300

    + order timer

--- Assets/Scripts/StateMachines/Cashier/TakeOrderState.cs
+++ Assets/Scripts/StateMachines/Cashier/TakeOrderState.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class TakeOrderState : State
 {
     CashierStateMachine stateMachine;
+    float takeOrderTimer;
     public TakeOrderState(CashierStateMachine stateMachine)
     {
         this.stateMachine = stateMachine;
     }
     public override void Enter()
     {
-
+        takeOrderTimer = 5f;
+        stateMachine.radialTimer.StartTimer(takeOrderTimer);
     }
 
     public override void Tick(float deltaTime)
     {
+        takeOrderTimer = Mathf.Max(0f, takeOrderTimer -= deltaTime);
 
+        if (takeOrderTimer == 0f)
+        {
+            Debug.Log("Order Taken");
+            //Order order = stateMachine.cashierStation.TakeOrder();
+            //CookManager.Instance.ReceiveOrder(order);
+            stateMachine.SwitchState(new IdleState(stateMachine));
+        }
     }
 
     public override void Exit()
