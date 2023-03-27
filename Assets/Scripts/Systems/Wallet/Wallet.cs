6f379fe18033f14dfb2abd77f06b896afb7fe713
Hakan Ayhan <hakanayhnn@gmail.com>
2023-03-28 00:05:33 +0300

    + Wallet manager

---
+++ Assets/Scripts/Systems/Wallet/Wallet.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallet : MonoBehaviour
+{
+    public UIManager UIManager;
+    public double goldAmount;
+
+    private void Start()
+    {
+        UIManager.SetGoldText(goldAmount);
+    }
+    public void AddGold(double gold)
+    {
+        goldAmount += gold;
+        UIManager.SetGoldText(goldAmount);
+    }
+}
