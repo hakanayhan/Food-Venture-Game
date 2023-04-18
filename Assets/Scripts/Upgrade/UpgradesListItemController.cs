e8ad27eff268e1503c232eff03b0beb1b21360fb
Hakan Ayhan <hakanayhnn@gmail.com>
2023-04-18 21:53:23 +0300

    + upgrade feature to increase maximum cashier and customer + automatic upgrade description generation feature

--- Assets/Scripts/Upgrade/UpgradesListItemController.cs
+++ Assets/Scripts/Upgrade/UpgradesListItemController.cs
@@ -6,21 +6,47 @@ using UnityEngine.UI;
 public class UpgradesListItemController : MonoBehaviour
 {
     public Upgrades upgrades;
-    [SerializeField] private Image icon;
-    [SerializeField] private Text title;
-    [SerializeField] private Text description;
-    [SerializeField] private Text priceText;
+    [SerializeField] private Image _icon;
+    [SerializeField] private Text _title;
+    [SerializeField] private Text _description;
+    [SerializeField] private Text _priceText;
 
     private void Start()
     {
-        icon.sprite = upgrades.icon;
-        title.text = upgrades.upgradeTitle;
-        description.text = upgrades.upgradeText;
-        priceText.text = upgrades.price.ToString();
+        _icon.sprite = upgrades.icon;
+        _title.text = upgrades.upgradeTitle;
+        _description.text = GetDescriptionText();
+        _priceText.text = new Currency(upgrades.price).ToShortString();
     }
 
-    public void upgradeButton()
+    private string GetDescriptionText()
     {
-        Debug.Log("Upgraded");
+        if (upgrades.feature == Upgrades.Features.addCashier)
+        {
+            string s = (upgrades.addRate > 1) ? "s" : "";
+            return "+" + upgrades.addRate + " Cashier" + s;
+        }
+        else if (upgrades.feature == Upgrades.Features.addCustomer)
+        {
+            string s = (upgrades.addRate > 1) ? "s" : "";
+            return "+" + upgrades.addRate + " Customer" + s;
+        }
+        return null;
+    }
+
+    public void UpgradeButton()
+    {
+        if (Wallet.Instance.TryRemoveGold(upgrades.price))
+        {
+            if(upgrades.feature == Upgrades.Features.addCashier)
+            {
+                CashierManager.Instance.maxCashier += upgrades.addRate;
+            }
+            else if(upgrades.feature == Upgrades.Features.addCustomer)
+            {
+                CustomerManager.Instance.maxCustomer += upgrades.addRate;
+            }
+            Destroy(this.gameObject);
+        }
     }
 }
