69deb76d854b5d78dab6c0727f313c5eb058cfda
Hakan Ayhan <hakanayhnn@gmail.com>
2023-04-18 04:07:41 +0300

    set camera controller off when upgrade window is active

--- Assets/Scripts/UI/UpgradesWindow.cs
+++ Assets/Scripts/UI/UpgradesWindow.cs
@@ -6,13 +6,16 @@ using UnityEngine.UI;
 public class UpgradesWindow : MonoBehaviour
 {
     public GameObject window;
+    public Camera cameraGameObject;
     public void OpenUpgrades()
     {
         window.SetActive(true);
+        cameraGameObject.GetComponent<CameraController>().enabled = false;
     }
 
     public void CloseUpgrades()
     {
         window.SetActive(false);
+        cameraGameObject.GetComponent<CameraController>().enabled = true;
     }
 }
