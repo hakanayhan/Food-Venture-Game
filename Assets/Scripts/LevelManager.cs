3d5c507eddeddc2c83489e30f25147676f874689
Hakan Ayhan <hakanayhnn@gmail.com>
2023-08-11 09:33:35 +0300

    + basic level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance; 
+    public int currentLv;
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    public void UpgradeLevel()
+    {
+        if (IsNextLvExist())
+            SceneManager.LoadScene(currentLv + 1);
+    }
+    
+    public bool IsNextLvExist()
+    {
+        return (SceneManager.sceneCountInBuildSettings > currentLv + 1);
+    }
+}
