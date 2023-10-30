ae102b39ba6df62fdb5ac5df8eb2369f874b7f17
HAKANS-RAZER\hakan <hakanayhnn@gmail.com>
2023-10-31 01:53:12 +0300

    + chef station

---
+++ Assets/Scripts/ChefStation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChefStation : MonoBehaviour
+{
+    public Order order;
+    public bool isReservedByCashier;
+    public bool isReservedByCook;
+    public Transform CashierTransform;
+    public Transform CookTransform;
+    public bool hasCustomer;
+    public bool hasOrder;
+    public CookStateMachine cook;
+
+    public void ReserveStation(CookStateMachine cook)
+    {
+        isReservedByCook = true;
+    }
+    public void ReserveStation(CashierStateMachine cashier)
+    {
+        isReservedByCashier = true;
+    }
+
+    public void ServeOrder(Order order)
+    {
+        this.order = order;
+        hasOrder = true;
+    }
+}
